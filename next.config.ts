add2106059318531e938253d660840804473dbd4
Carlos Bueno <carlosbueno.contato@gmail.com>
2025-06-17 17:34:01 -0300

    test

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: 'standalone',
-  // No env section needed
+  // No env section needed.
   // pageExtensions: ['page.tsx', 'page.ts'],  <-- REMOVE THIS LINE
 };
 
